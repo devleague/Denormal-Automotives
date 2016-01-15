123b8f8084ca8e18ce07dc515c2b1edb698152cb
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-14 19:40:17 -1000

    Qs 4, 5, 6 passed

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -1,4 +1,12 @@
-CREATE USER denormal_user;
+-- 4
+-- SELECT *
+-- FROM car_models;
 
-CREATE DATABASE denormal_cars WITH OWNER = denormal_user;
+-- 5
+-- SELECT DISTINCT make_title
+-- FROM car_models;
 
+-- 6
+SELECT DISTINCT model_title
+FROM car_models
+WHERE make_code LIKE 'VOLKS';
