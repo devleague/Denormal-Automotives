a2da9b59427fc780ca7e754f4d788724da43d533
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-16 13:13:53 -1000

    everything working

--- normal_cars.sql
+++ normal_cars.sql
@@ -4,24 +4,24 @@
 -- make_title varchar(125) NOT NULL
 -- );
 
--- CREATE TABLE models (
--- id serial primary key NOT NULL,
--- model_code varchar(125) NOT NULL,
--- model_title varchar(125) NOT NULL,
--- year int NOT NULL,
--- make_id int references makes(id)
--- );
+CREATE TABLE models (
+id serial primary key NOT NULL,
+model_code varchar(125) NOT NULL,
+model_title varchar(125) NOT NULL,
+year int NOT NULL,
+make_id int references makes(id)
+);
 
 -- INSERT INTO makes (make_code, make_title)
 -- SELECT DISTINCT make_code, make_title
 -- FROM car_models
 -- ORDER BY make_code ASC;
 
--- INSERT INTO models (model_code, model_title, year, make_id)
--- SELECT car_models.model_code, car_models.model_title, car_models.year, makes.id
--- FROM car_models
---   INNER JOIN makes ON (makes.make_code = car_models.make_code)
--- ORDER BY car_models.year ASC;
+INSERT INTO models (model_code, model_title, year, make_id)
+SELECT car_models.model_code, car_models.model_title, car_models.year, makes.id
+FROM car_models
+  INNER JOIN makes ON (makes.make_code = car_models.make_code)
+ORDER BY car_models.year ASC;
 
 -- -- 7
 -- SELECT make_title
