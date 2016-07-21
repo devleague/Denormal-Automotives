fa2c1e100230088710ba31d2c3095a69514f80e8
Lisa <misslisazhou@gmail.com>
2016-07-20 21:58:28 -1000

    Create query to list model_title values where make_code is 'VOLKS' w/o dupes

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -12,5 +12,11 @@ CREATE DATABASE denormal_cars WITH OWNER denormal_user;
 \i scripts/denormal_data.sql;
 
 \dS car_models;
+
+-- Create a query to get a list of all make_title values in the car_models table, without any duplicate rows. (should have 71 results)
 SELECT DISTINCT make_title from car_models;
 SELECT COUNT(DISTINCT make_title) FROM car_models;
+
+-- Create a query to list all model_title values where the make_code is 'VOLKS', without any duplicate rows (should have 27 results)
+SELECT DISTINCT model_title from car_models WHERE make_code = 'VOLKS';
+SELECT COUNT(DISTINCT model_title) from car_models WHERE make_code = 'VOLKS';
