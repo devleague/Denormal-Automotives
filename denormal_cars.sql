912685398037d6bacb005ceccc121dcb6653facc
Lisa <misslisazhou@gmail.com>
2016-07-20 22:00:47 -1000

    Create query to list all from rom car_models where the make_code is 'LAM'

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -20,3 +20,7 @@ SELECT COUNT(DISTINCT make_title) FROM car_models;
 -- Create a query to list all model_title values where the make_code is 'VOLKS', without any duplicate rows (should have 27 results)
 SELECT DISTINCT model_title from car_models WHERE make_code = 'VOLKS';
 SELECT COUNT(DISTINCT model_title) from car_models WHERE make_code = 'VOLKS';
+
+-- Create a query to list all make_code, model_code, model_title, and year from car_models where the make_code is 'LAM' (should have 136 rows)
+SELECT make_code, model_code, model_title, year from car_models WHERE make_code = 'LAM';
+SELECT COUNT(make_code) from car_models WHERE make_code = 'LAM';
