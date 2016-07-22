db16e1acaed28200277a6a5500b09dbae0554013
Nikki K <808nikz@gmail.com>
2016-07-21 21:33:36 -1000

    Inserted model_code & model_title into models table

--- normal_cars.sql
+++ normal_cars.sql
@@ -10,18 +10,46 @@ CREATE DATABASE normal_cars WITH OWNER normal_user;
 
 CREATE TABLE makes (
   id SERIAL PRIMARY KEY,
-  make_code VARCHAR(255) NOT NULL,
-  make_title VARCHAR(255) NOT NULL
+  make_code VARCHAR(125) NOT NULL,
+  make_title VARCHAR(125) NOT NULL
 );
 
 CREATE TABLE models (
   id SERIAL PRIMARY KEY,
-  model_code VARCHAR(255) NOT NULL,
-  model_title VARCHAR(255) NOT NULL,
-  make_id INT REFERENCES makes(id)
+  model_code VARCHAR(125) NOT NULL,
+  model_title VARCHAR(125) NOT NULL
 );
 
 CREATE TABLE year (
   id SERIAL PRIMARY KEY,
   year_value INT
 );
+
+CREATE TABLE cars (
+  id SERIAL PRIMARY KEY,
+  make_id INT REFERENCES makes(id),
+  model_id INT REFERENCES models(id),
+  year_value INT
+);
+
+INSERT INTO makes
+  (make_code,
+    make_title)
+  SELECT DISTINCT make_code, make_title
+    FROM car_models
+    ORDER BY make_code ASC;
+
+INSERT INTO models
+  (model_code,
+    model_title)
+  SELECT DISTINCT model_code, model_title
+    FROM car_models
+    ORDER BY model_code ASC;
+
+-- SELECT COUNT(DISTINCT model_title)
+--   FROM car_models
+--   WHERE make_code = 'VOLKS';
+
+-- SELECT COUNT(*)
+--   FROM car_models
+--   WHERE make_code = 'LAM';
