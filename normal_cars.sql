d9687b4927c1d79b43abcdde40a94df1ab7260dd
Lisa <misslisazhou@gmail.com>
2016-07-22 21:15:07 -1000

    create normalized tables in normal_cars database

--- normal_cars.sql
+++ normal_cars.sql
@@ -21,7 +21,6 @@ CREATE TABLE IF NOT EXISTS make
 CREATE TABLE IF NOT EXISTS model
 (
   id SERIAL PRIMARY KEY,
-  make_id INTEGER REFERENCES make(id),
   model_code character varying(125) NOT NULL,
   model_title character varying(125) NOT NULL
 );
@@ -33,5 +32,32 @@ CREATE TABLE IF NOT EXISTS year
   year integer NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS car_list
+(
+  id SERIAL PRIMARY KEY,
+  make_id INTEGER REFERENCES make(id),
+  model_id INTEGER REFERENCES model(id),
+  year_id INTEGER REFERENCES year(id)
+);
+
 INSERT INTO make (make_code, make_title)
   SELECT DISTINCT make_code, make_title FROM car_models ORDER BY make_code ASC;
+
+INSERT INTO model (model_code, model_title)
+  SELECT DISTINCT model_code, model_title FROM car_models ORDER BY model_code ASC;
+
+INSERT INTO year (year)
+  SELECT DISTINCT year FROM car_models ORDER BY year ASC;
+
+INSERT INTO car_list (make_id, model_id, year_id)
+  SELECT make.id AS make_id, model.id AS model_id, year.id AS year_id FROM car_models
+    INNER JOIN make on make.make_code = car_models.make_code
+    INNER JOIN model on model.model_code = car_models.model_code
+    INNER JOIN year on year.year = car_models.year;
+
+SELECT * FROM car_list;
+
+
+-- SELECT * FROM make;
+-- SELECT * FROM model;
+-- SELECT * FROM year;
